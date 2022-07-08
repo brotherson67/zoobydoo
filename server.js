204ed9b0d2697a40f804d9e84382ed2e442979e5
brotherson67 <mitchell.brotherson@gmail.com>
2022-07-07 22:37:38 -0600

    created POST method and data validation

--- server.js
+++ server.js
@@ -1,11 +1,20 @@
 // get animal data
 const { animals } = require("./data/animal_data.json");
+// install file system manager
+const fs = require("fs");
+const path = require("path");
 //install expressjs
 const express = require("express");
 // create instance of express app
 const app = express();
 // Define Port app will run on
 const PORT = process.env.PORT || 3000;
+
+// express middleware
+// parse incoming string or array data
+app.use(express.urlencoded({ extended: true }));
+//parse incoming JSON data
+app.use(express.json());
 
 // query handler function
 function filterByQuery(query, animalsArray) {
@@ -61,6 +70,43 @@ function findById(id, animalsArray) {
   return result;
 }
 
+// function to create a new animal
+function createNewAnimal(body, animalsArray) {
+  // define the animal as the request payload
+  const animal = body;
+  // add the animal to the animals array
+  animalsArray.push(animal);
+  // write new array to the source file
+  fs.writeFileSync(
+    path.join(__dirname, "./data/animal_data.json"),
+    JSON.stringify({ animals: animalsArray }, null, 2)
+  );
+  // return finished code to POST route for response
+  return animal;
+}
+
+// data validation function
+function validateAnimal(animal) {
+  // make sure the animal name is a string
+  if (!animal.name || typeof animal.name !== "string") {
+    return false;
+  }
+  // make sure the animal species is a string
+  if (!animal.species || typeof animal.species !== "string") {
+    return false;
+  }
+  // make sure animal diet is a string
+  if (!animal.diet || typeof animal.diet !== "string") {
+    return false;
+  }
+  // make sure personality traits is in an array
+  if (!animal.personalityTraits || !Array.isArray(animal.personalityTraits)) {
+    return false;
+  }
+  // return true if it's passed all the validations
+  return true;
+}
+
 // animal route
 app.get("/api/animals", (req, res) => {
   // create local instance of animal array
@@ -80,6 +126,20 @@ app.get("/api/animals/:id", (req, res) => {
     // if there is not a matching animal, send 404
   } else {
     res.send(404);
+  }
+});
+
+// animal creation route
+app.post("/api/animals", (req, res) => {
+  // set the new animal id
+  req.body.id = animals.length.toString();
+  // if there is anything incorrect about the payload, return 400
+  if (!validateAnimal(req.body)) {
+    res.status(400).send("the animal is not formatted correctly");
+  } else {
+    // add animal to json file and animals array in this function
+    const animal = createNewAnimal(req.body, animals);
+    res.json(req.body);
   }
 });
 
